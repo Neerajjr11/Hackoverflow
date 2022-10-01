36f1afc65c841bc45e3865457149635a6ccdcc01
Abhay Arora <abhayarorasap123@gmail.com>
2022-10-01 10:32:55 +0530

    Initial Frontend Files

---
+++ frontend/components/homeBanner/HomeBanner.js
@@ -0,0 +1,7 @@
+const HomeBanner = () =>{
+	return (
+		<h1>diashdsah</h1>
+	);
+}
+
+export default HomeBanner;
---
+++ frontend/components/navbar/Navbar.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="max-w mx-auto px-4">
+        <div className="flex">
+          <div className="hidden md:flex nav-left w-1/2 justify-center space-x-16 p-6">
+            <div>Home</div>
+            <div>About Us</div>
+            <div>Contact Us</div>
+          </div>
+          <div className="brand-name ">
+            <Image
+              src="/1t-logo.png"
+              width={150}
+              height={200}
+              alt="PlaceINDIA Logo"
+			  draggable="false"
+            ></Image>
+          </div>
+          <div className="nav-right w-1/2 flex justify-center space-x-16 p-6">
+            <div>
+				hello
+			</div>
+			<div className="w-1/3">
+              <input
+                type="text"
+                id="search-navbar"
+                
+                placeholder="Search..."
+              />
+            </div>
+			
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
