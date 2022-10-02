245733a937bfa6f44b19589002a015ded3326b3b
Abhay Arora <abhayarorasap123@gmail.com>
2022-10-02 06:18:36 +0530

    Updated Navbar

--- frontend/components/navbar/Navbar.js
+++ frontend/components/navbar/Navbar.js
@@ -1,7 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useState, useEffect } from 'react';
 
 const Navbar = () => {
+	const [collegeDetails, setCollegeDetails]= useState({
+        college_id: 0,
+        name: "All",
+        region: "India", 
+        branch: "All"
+    })
+
+	useEffect(()=>{
+        fetch("http://127.0.0.1:8000/api/get_colleges").then(res=>res.json())
+        .then(res=>{
+            setColleges(res.data)
+        });
+    }, [])
+
+	const [college, setColleges] = useState([]);
+    const [college_id, setCollegeId] = useState(1);
   return (
     <nav>
       <div className="max-w mx-auto px-4">
@@ -22,14 +39,15 @@ const Navbar = () => {
           </div>
           <div className="nav-right w-1/2 flex justify-center space-x-16 p-6">
             <div>
-				hello
+			<button className='rounded-[5px] shadow-md hover:shadow-xl text-[1.4rem] bg-[#584EFD] px-10 py-5 font-poppins font-md text-white py-2'>My Data</button>
 			</div>
 			<div className="w-1/3">
-              <input
-                type="text"
-                id="search-navbar"
-                placeholder="Search..."
-              />
+			<select className="w-[30%] mr-60 rounded-xl border-0 bg-[#fff] h-[40px] text-[1.5rem]" onChange={e=>{
+                        setStatistics(["...", "...", "...", "..."])
+                        setCollegeId(e.target.value)
+                        }}>
+                        {college.map((val, index)=><option value={val.college_id}>{val.name} ({val.state})</option>)}
+                    </select>
             </div>
 			
           </div>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,7 @@
 import HomeBanner from "../components/homeBanner/HomeBanner";
 import TopInstitutes from "../components/institutes/TopInstitutes";
 import Navbar from "../components/navbar/Navbar";
-import {
-	Table,
-	Thead,
-	Tbody,
-	Tfoot,
-	Tr,
-	Th,
-	Td,
-	TableCaption,
-  } from "@chakra-ui/react"
+
 import DatamapsIndia from 'react-datamaps-india'
 
 const HomePage = () => {
